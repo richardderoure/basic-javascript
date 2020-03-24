5a0f434b61723ab3f8e39080d7c6d6e2cd3fba5f
richard <r.d.deroure@se16.qmul.ac.uk>
2020-03-24 16:02:53 +0000

    updated file with post lunch work

--- ternary-testing.js
+++ ternary-testing.js
@@ -5,7 +5,6 @@ function ternaryOutput(item, count) {
 console.log(ternaryOutput('pie', 1));
 console.log(ternaryOutput('pie', 2));
 
-
 function patternOutput() {
     let myStr = "";
     for(let i = 1; i<51; i++){
@@ -14,5 +13,56 @@ function patternOutput() {
     }
 return myStr;
 }
-
 console.log(patternOutput());
+
+
+let exampleString = 'Hello world this is a test!';
+console.log(exampleString.length);
+
+let str1 =  'I dislike trains as a mode of transport';
+let arr1 = str1.split(" ");
+let str2 = arr1[2].split("");
+str2.pop();
+console.log(str2.join(''));
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+console.log(alphabet.split('')[4]);
+
+
+let str3 = 'I love you';
+console.log(str3.toUpperCase());
+
+let num1 = 1.23456789;
+console.log(num1.toFixed(2));
+
+console.log(Number("45"));
+
+let arr2 = ['red', 'yellow', 'green', 'blue'];
+
+console.log(arr2.shift(), arr2.pop());
+arr2.push('orange');
+console.log(arr2);
+console.log(arr2[2]);
+
+let arr3 = [23123, 2, -328, 0];
+
+let value = Infinity;
+for(i=0; i < arr3.length; i++){
+    if (arr3[i]<value){value = arr3[i]}
+}
+console.log(value);
+
+
+travel = (country,length,place = 'London') => { console.log(`This imaginary person flew to ${place} from ${country}. The flight took a total of ${length} hours.`) };
+
+travel('France', 2, 'Italy');
+travel('France', 2);
+
+function factorial(n){
+    let x = 1;
+    for(i=1; i<=n; i++){
+        x*=i;
+    }
+    return x
+}
+
+console.log(factorial(3));
